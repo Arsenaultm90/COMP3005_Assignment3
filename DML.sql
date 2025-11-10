fe07f8dd022ae730590749cf2a830e1fe268ec7a
Matt Arsenault <Matthew.B.Arsenault@gmail.com>
2025-11-10 08:41:39 -0500

    Assignment 3

---
+++ DML.sql
@@ -0,0 +1,12 @@
+CREATE TABLE public.students (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    enrollment_date DATE NOT NULL
+);
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
